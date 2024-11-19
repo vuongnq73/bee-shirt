d73f79d0bc1ac4ba84795d5ec63dc844afcd2e05
vuongnq73 <145737558+vuongnq73@users.noreply.github.com>
2024-11-19 11:41:17 +0700

    uptofullcode

---
+++ src/main/java/com/example/bee_shirt/controller/ShirtDetailController.java
@@ -0,0 +1,112 @@
+package com.example.bee_shirt.controller;
+
+import com.example.bee_shirt.EntityThuocTinh.*;
+import com.example.bee_shirt.dto.ShirtDetailDTO;
+import com.example.bee_shirt.entity.Shirt;
+import com.example.bee_shirt.entity.ShirtDetail;
+import com.example.bee_shirt.service.lmp.ShirtDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shirt-details")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+public class ShirtDetailController {
+
+    @Autowired
+    private ShirtDetailService shirtDetailService;
+
+    @GetMapping("/api/colors")
+    public ResponseEntity<Iterable<Color>> getAllColors() {
+        Iterable<Color> colors = shirtDetailService.getAllColors();
+        return ResponseEntity.ok(colors);
+    }
+
+    @GetMapping("/api/genders")
+    public ResponseEntity<Iterable<Gender>> getAllGenders() {
+        Iterable<Gender> genders = shirtDetailService.getAllGenders();
+        return ResponseEntity.ok(genders);
+    }
+
+    @GetMapping("/api/patterns")
+    public ResponseEntity<Iterable<Pattern>> getAllPatterns() {
+        Iterable<Pattern> patterns = shirtDetailService.getAllPatterns();
+        return ResponseEntity.ok(patterns);
+    }
+
+    @GetMapping("/api/sizes")
+    public ResponseEntity<Iterable<Size>> getAllSizes() {
+        Iterable<Size> sizes = shirtDetailService.getAllSizes();
+        return ResponseEntity.ok(sizes);
+    }
+
+    @GetMapping("/api/materials")
+    public ResponseEntity<Iterable<Material>> getAllMaterials() {
+        Iterable<Material> materials = shirtDetailService.getAllMaterials();
+        return ResponseEntity.ok(materials);
+    }
+
+    @GetMapping("/api/seasons")
+    public ResponseEntity<Iterable<Season>> getAllSeasons() {
+        Iterable<Season> seasons = shirtDetailService.getAllSeasons();
+        return ResponseEntity.ok(seasons);
+    }
+    @GetMapping("/api/origins")
+    public ResponseEntity<Iterable<Origin>> getAllOrigins() {
+        Iterable<Origin> origins = shirtDetailService.getAllOrigins();
+        return ResponseEntity.ok(origins);
+    }
+    @GetMapping("/api/shirts")
+    public ResponseEntity<Iterable<Shirt>> getAllShirts() {
+        Iterable<Shirt> shirts = shirtDetailService.getAllShirts();
+        return ResponseEntity.ok(shirts);
+    }
+    // Hiển thị danh sách chi tiết áo thun với phân trang
+    @GetMapping("/api/hienthi")
+    public ResponseEntity<List<ShirtDetailDTO>> getShirtDetails(Pageable pageable) {
+        Page<ShirtDetailDTO> results = shirtDetailService.getAllShirtDetails(pageable);
+        return ResponseEntity.ok(results.getContent());
+    }
+    // Thêm chi tiết áo thun
+    @PostMapping("/add")
+    public ResponseEntity<ShirtDetail> addShirtDetail(@RequestBody ShirtDetail shirtDetail) {
+        ShirtDetail createdShirtDetail = shirtDetailService.addShirtDetail(shirtDetail);
+        return ResponseEntity.ok(createdShirtDetail);
+    }
+    // Cập nhật chi tiết áo thun
+    @PutMapping("/update/{codeShirtDetail}")
+    public ResponseEntity<ShirtDetail> updateShirtDetail(@PathVariable String codeShirtDetail, @RequestBody ShirtDetail updatedShirtDetail) {
+        ShirtDetail shirtDetail = shirtDetailService.updateShirtDetail(codeShirtDetail, updatedShirtDetail);
+        if (shirtDetail == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(shirtDetail);
+    }
+    @GetMapping("/detail/{codeShirtDetail}")
+    public ResponseEntity<ShirtDetailDTO> getShirtDetail(@PathVariable String codeShirtDetail) {
+        ShirtDetailDTO shirtDetail = shirtDetailService.getShirtDetail(codeShirtDetail);
+        if (shirtDetail != null) {
+            return ResponseEntity.ok(shirtDetail);
+        } else {
+            // Trả về 404 nếu không tìm thấy
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    // Xóa chi tiết áo thun
+    @DeleteMapping("/delete/{codeShirtDetail}")
+    public ResponseEntity<Void> deleteShirtDetail(@PathVariable String codeShirtDetail) {
+        ShirtDetail shirtDetail = shirtDetailService.deleteShirtDetail(codeShirtDetail);
+        if (shirtDetail!=null) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/example/bee_shirt/exception/ErrorCode.java
@@ -0,0 +1,44 @@
+package com.example.bee_shirt.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INVALID_KEY(1001,"Invalid message key", HttpStatus.UNAUTHORIZED),
+
+    USER_EXISTED(1002,"Username existed",HttpStatus.BAD_REQUEST),
+
+    USER_NOT_EXISTED(1003,"Account not existed",HttpStatus.NOT_FOUND),
+
+    UNAUTHENTICATED(1004,"UnAuthenticated",HttpStatus.BAD_REQUEST),
+
+    USERNAME_INVALID(1005,"Username must be at least 3 characters",HttpStatus.BAD_REQUEST),
+
+    PASSWORD_INVALID(1006,"Password must be at least 5 characters",HttpStatus.BAD_REQUEST),
+
+    UNAUTHORIZED(1007,"You do not have role",HttpStatus.FORBIDDEN),
+
+    TOKEN_INVALID(1008,"Token invalid",HttpStatus.BAD_REQUEST),
+
+    ROLE_NOT_FOUND(1009,"Role not found",HttpStatus.NOT_FOUND),
+
+    ACCOUNT_ALREADY(1010,"Account has been already",HttpStatus.ALREADY_REPORTED),
+
+    FILE_UPLOAD_FAILED(1011,"File upload has been fail",HttpStatus.BAD_REQUEST),
+
+
+    INVALID_FILE_TYPE(1012,"This file is not image",HttpStatus.BAD_REQUEST),
+
+    ACCOUNT_NOT_FOUND(1012,"Account not found",HttpStatus.NOT_FOUND)
+
+    ;
+    private int code;
+
+    private String message;
+
+    private HttpStatusCode statusCode;
+}
