b305a1a4e6a4f275530f749e98de498ab8a464dc
vuongnq73 <145737558+vuongnq73@users.noreply.github.com>
2024-11-19 11:39:39 +0700

    Emtry

--- src/main/java/com/example/bee_shirt/repository/PaymentMethodRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.bee_shirt.repository;
-
-import com.example.bee_shirt.entity.PaymentMethod;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentMethodRepo extends JpaRepository<PaymentMethod,Integer> {
-}
