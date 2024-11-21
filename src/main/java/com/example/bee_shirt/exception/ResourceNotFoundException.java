3b8a5c8b9d9d77ac72c155fb8bcd2f1a47d43b10
giangdt04 <giangdtph40542@gmail.com>
2024-11-22 03:28:33 +0700

    commit

--- src/main/java/com/example/bee_shirt/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.bee_shirt.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-}
