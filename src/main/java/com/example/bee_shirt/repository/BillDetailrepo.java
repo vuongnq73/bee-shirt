375bdda6f427891aa9973c8046a2fc6dff352105
nguyly2004 <150369740+nguyly2004@users.noreply.github.com>
2024-11-21 14:26:48 +0700

    voucher commit1

--- src/main/java/com/example/bee_shirt/repository/BillDetailrepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.bee_shirt.repository;
-
-import com.example.bee_shirt.entity.BillDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillDetailrepo extends JpaRepository<BillDetail,Integer> {
-}
