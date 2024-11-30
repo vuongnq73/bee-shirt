7361d061ff28ea01daa9315e4857131901802dec
vuongnq73 <145737558+vuongnq73@users.noreply.github.com>
2024-12-01 01:07:44 +0700

    full code

---
+++ src/main/java/com/example/bee_shirt/dto/ShirtDetailDTO.java
@@ -0,0 +1,42 @@
+package com.example.bee_shirt.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ShirtDetailDTO {
+    private int id;
+    private String codeShirtDetail;
+    private String codeShirt;
+    private String nameShirt;
+
+    private BigDecimal price;
+    private int quantity;
+    private String namePattern;
+    private String nameGender;
+    private String nameOrigin;
+    private String nameSeason;
+    private String nameSize;
+    private String nameMaterial;
+    private String nameColor;
+    private int statusshirtdetail;
+    private String createBy;
+    private Date createAt;
+    private String updateBy;
+    private Date updateAt;
+    private boolean deleted;
+    private int shirtId;
+    private int patternId;
+    private int genderId;
+    private int originId;
+    private int seasonId;
+    private int sizeId;
+    private int materialId;
+    private int colorId;
+}
---
+++ src/main/java/com/example/bee_shirt/repository/ShirtDetailRepository.java
@@ -0,0 +1,78 @@
+package com.example.bee_shirt.repository;
+
+import com.example.bee_shirt.dto.ShirtDetailDTO;
+import com.example.bee_shirt.entity.ShirtDetail;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ShirtDetailRepository extends JpaRepository<ShirtDetail, Integer> {
+    @Query("SELECT sd FROM ShirtDetail sd WHERE sd.codeShirtDetail LIKE %:query%")
+    ShirtDetail findShirtDetailByCode(@Param("query") String query);
+    //
+
+    @Query("SELECT sd FROM ShirtDetail sd WHERE sd.codeShirtDetail LIKE %:query% OR sd.shirt.nameshirt LIKE %:query%")
+    Page<ShirtDetail> findListShirtDetailByCodeOrName(@Param("query") String query, Pageable pageable);
+
+    // Truy vấn tất cả chi tiết áo không bị xóa và có trạng thái 1
+    @Query("SELECT new com.example.bee_shirt.dto.ShirtDetailDTO(" +
+            "sdt.id, sdt.codeShirtDetail,ss.codeshirt, CONCAT(ss.nameshirt, '[ ', c.nameColor, '+ ', si.namesize,']'), sdt.price, sdt.quantity, p.namePattern, " +
+            "g.nameGender, o.nameOrigin, s.nameSeason, si.namesize, m.nameMaterial, c.nameColor, sdt.statusshirtdetail, " +
+            "sdt.createBy, sdt.createAt, sdt.updateBy, sdt.updateAt, sdt.deleted,ss.id, p.id, g.id, o.id, s.id, si.id, m.id, c.id) " +
+            "FROM ShirtDetail sdt " +
+            "JOIN sdt.shirt ss " +
+            "JOIN sdt.pattern p " +
+            "JOIN sdt.gender g " +
+            "JOIN sdt.origin o " +
+            "JOIN sdt.season s " +
+            "JOIN sdt.size si " +
+            "JOIN sdt.material m " +
+            "JOIN sdt.color c " +
+            "ORDER BY sdt.id DESC")
+    List<ShirtDetailDTO> findAllShirtDetails();
+
+
+    // Tìm chi tiết áo theo mã chi tiết
+    ShirtDetail findByCodeShirtDetail(String codeShirtDetail);
+
+    // Truy vấn chi tiết áo theo mã không bị xóa và có trạng thái 0
+    @Query("SELECT new com.example.bee_shirt.dto.ShirtDetailDTO(" +
+            "sdt.id, sdt.codeShirtDetail,ss.codeshirt, CONCAT(ss.nameshirt, ' ', c.nameColor, ' ', si.namesize), sdt.price, sdt.quantity, p.namePattern, g.nameGender, o.nameOrigin, " +
+            "s.nameSeason, si.namesize, m.nameMaterial, c.nameColor, sdt.statusshirtdetail, sdt.createBy, " +
+            "sdt.createAt, sdt.updateBy, sdt.updateAt, sdt.deleted,ss.id,p.id,g.id,o.id,s.id,si.id,m.id,c.id) " +
+            "FROM ShirtDetail sdt " +
+            "JOIN sdt.shirt ss " +
+            "JOIN sdt.pattern p " +
+            "JOIN sdt.gender g " +
+            "JOIN sdt.origin o " +
+            "JOIN sdt.season s " +
+            "JOIN sdt.size si " +
+            "JOIN sdt.material m " +
+            "JOIN sdt.color c " +
+            "WHERE sdt.codeShirtDetail = ?1" +
+            "ORDER BY sdt.id DESC")
+    Page<ShirtDetailDTO> findAllShirtDetailByName(Pageable pageable);
+    // Truy vấn chi tiết áo theo mã áo
+    @Query("SELECT new com.example.bee_shirt.dto.ShirtDetailDTO(" +
+            "sdt.id, sdt.codeShirtDetail,ss.codeshirt, CONCAT(ss.nameshirt, ' ', c.nameColor, ' ', si.namesize), sdt.price, sdt.quantity, p.namePattern, g.nameGender, o.nameOrigin, " +
+            "s.nameSeason, si.namesize, m.nameMaterial, c.nameColor, sdt.statusshirtdetail, sdt.createBy, " +
+            "sdt.createAt, sdt.updateBy, sdt.updateAt, sdt.deleted, ss.id, p.id, g.id, o.id, s.id, si.id, m.id, c.id) " +
+            "FROM ShirtDetail sdt " +
+            "JOIN sdt.shirt ss " +
+            "JOIN sdt.pattern p " +
+            "JOIN sdt.gender g " +
+            "JOIN sdt.origin o " +
+            "JOIN sdt.season s " +
+            "JOIN sdt.size si " +
+            "JOIN sdt.material m " +
+            "JOIN sdt.color c " +
+            "WHERE ss.codeshirt = :codeshirt " +
+            "ORDER BY sdt.id DESC")
+    List<ShirtDetailDTO> findAllShirtDetailByCodeShirt(@Param("codeshirt") String codeshirt);
+
+
+}
