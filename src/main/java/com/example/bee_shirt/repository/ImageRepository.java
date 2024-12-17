26e7b688b215a267a724e661a766c983f34ff7d0
vuongnq73 <145737558+vuongnq73@users.noreply.github.com>
2024-12-17 14:42:46 +0700

    emtry

--- src/main/java/com/example/bee_shirt/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bee_shirt.repository;
-
-import com.example.bee_shirt.EntityThuocTinh.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Integer> {
-}
