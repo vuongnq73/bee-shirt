66f82a887827b0f429fd45b4b66420b28e9f6697
vuongnq73 <145737558+vuongnq73@users.noreply.github.com>
2024-12-28 16:17:51 +0700

    emtry

--- DATN.sql
+++
@@ -1,1 +0,0 @@
-select * from shirt_detail
--- src/main/java/com/example/bee_shirt/repository/InvalidatedTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bee_shirt.repository;
-
-import com.example.bee_shirt.entity.InvalidatedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
-}
